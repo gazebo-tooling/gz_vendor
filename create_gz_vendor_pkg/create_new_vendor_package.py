f240c9d6f182242407865e259a98693b34685a5f
Addisu Z. Taddese <addisu@openrobotics.org>
2024-02-22 22:20:35 -0600

    Move scripts and templates into 'create_gz_vendor'
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

---
+++ create_gz_vendor_pkg/create_new_vendor_package.py
@@ -0,0 +1,42 @@
+import argparse
+import sys
+from catkin_pkg.package import parse_package_string
+from create_vendor_packages import (remove_version, create_vendor_name, generate_vendor_package_files)
+import os
+import shutil
+from pathlib import Path
+
+
+def main(argv=sys.argv[1:]):
+    parser = argparse.ArgumentParser(
+        description='Parse package.xml file and generate a vendor package',
+    )
+    parser.add_argument(
+        'input_package_xml',
+        type=argparse.FileType('r', encoding='utf-8'),
+        help='The path to a package.xml file',
+    )
+    args = parser.parse_args(argv)
+    try:
+        package = parse_package_string(
+            args.input_package_xml.read(), filename=args.input_package_xml.name)
+    except Exception as e:
+        print("Error parsing '%s':" % args.input_package_xml.name, file=sys.stderr)
+        raise e
+    finally:
+        args.input_package_xml.close()
+
+    pkg_name_no_version = remove_version(package.name)
+    vendor_name = create_vendor_name(pkg_name_no_version)
+    os.mkdir(vendor_name)
+    generate_vendor_package_files(package, os.path.join(vendor_name))
+
+    templates_path = Path(__file__).resolve().parent / "templates"
+    # Copy other files
+    for file in ["LICENSE", "CONTRIBUTING.md"]:
+        shutil.copy(templates_path / file, Path(vendor_name) / file)
+
+    shutil.copy(templates_path / "extras.cmake.in", Path(vendor_name) / f"{vendor_name}-extras.cmake.in")
+
+if __name__ == "__main__":
+    main()
