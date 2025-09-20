6c011aaa6842bc80887a563a92a42805b7764e9b
Addisu Z. Taddese <addisu@openrobotics.org>
2025-09-19 17:44:59 -0700

    Add script to parse a gazebodistro file and create all vendor packages (#20)
    * Add script to parse a gazebodistro file and create all vendor packages
    
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>
    
    * Checkout the correct branch
    
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>
    
    * Add --output_dir arg and append package name
    
    Write each vendor package to a subfolder of the
    output_dir arg named after the vendor package.
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>
    
    * Vendor CLI11 in gz-utils (#22)
    
    CLI11 is not yet available on ci.ros2.org, so use the vendored version
    
    
    ---------
    
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>
    Co-authored-by: Steve Peters <computersthatmove@gmail.com>
    
    * Download collection file
    
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>
    
    ---------
    
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>
    Co-authored-by: Steve Peters <scpeters@openrobotics.org>
    Co-authored-by: Steve Peters <computersthatmove@gmail.com>

---
+++ create_gz_vendor_pkg/gz_vendor_all_libs.py
@@ -0,0 +1,70 @@
+import argparse
+from os.path import expanduser
+import sys
+from urllib.request import urlopen
+import tempfile
+import yaml
+import create_vendor_package
+from pathlib import Path
+from subprocess import run
+
+
+def clone(path, info):
+    run(["git", "clone", "--depth", "1", "-b", info["version"], info["url"], path])
+
+
+def get_collection(release):
+    collection_url = f"https://github.com/gazebo-tooling/gazebodistro/raw/master/collection-{release}.yaml"  # noqa
+    collection_yaml = urlopen(url=collection_url)
+    return yaml.full_load(collection_yaml)
+
+
+def get_collection_local(release):
+    with open(expanduser(f"~/ws/{release}/src/collection-{release}.yaml"), "r") as f:
+        return yaml.full_load(f.read())
+
+
+def main(argv=sys.argv[1:]):
+    parser = argparse.ArgumentParser(
+        description="Update all packages for a given Gazebo release",
+    )
+
+    parser.add_argument(
+        "gazebo_release",
+        type=str,
+        help="Name of Gazebo release to use",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        help="Output directory",
+    )
+    args, unknown_args = parser.parse_known_args(argv)
+    collection = get_collection(args.gazebo_release)
+
+    # Sparse clone all the repos first
+    with tempfile.TemporaryDirectory(prefix="gz_vendor_") as libs_path:
+        for name, info in collection["repositories"].items():
+            lib_path = Path(libs_path) / name
+            clone(lib_path, info)
+
+            package_xml_path = Path(lib_path) / "package.xml"
+            print(package_xml_path)
+
+            output_dir_args = []
+            if args.output_dir != "":
+                vendor_name = name.replace('-', '_') + '_vendor'
+                output_path = str(Path(args.output_dir) / vendor_name)
+                output_dir_args.append("--output_dir")
+                output_dir_args.append(output_path)
+
+            try:
+                create_vendor_package.main([str(package_xml_path), *output_dir_args, *unknown_args])
+            except Exception as e:
+                print("Error: ", e)
+                import traceback
+                traceback.print_exc()
+
+
+if __name__ == "__main__":
+    main()
